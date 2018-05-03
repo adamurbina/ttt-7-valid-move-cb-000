214144d8f080bd03586aaeeb208517da8d3e57b6
Adam Urbina <adamurbina@gmail.com>
2018-05-03 15:05:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-    if !(index in [0:9]) || position_taken(board, index)
+    if !(index.in?(0..8)) || position_taken(board, index)
         return false
     else
         return true
